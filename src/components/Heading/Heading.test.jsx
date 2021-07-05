059035a6cbf11a85f0e6cabe8710d6b452307ec6
Mauricio Sousa <morricedev@gmail.com>
2021-07-05 16:00:16 -0300

    Update Heading snapshot test

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -92,6 +92,6 @@ describe('<Heading />', () => {
   it('should match snapshot', () => {
     const { container } = renderTheme(<Heading as="h6">texto</Heading>);
 
-    expect(container).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
