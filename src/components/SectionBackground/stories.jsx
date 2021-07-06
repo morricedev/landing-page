50e588b7822d46f62bfad463488bc5491f7accbf
Mauricio Sousa <morricedev@gmail.com>
2021-07-06 13:21:51 -0300

    Created SectionBackground component

---
+++ src/components/SectionBackground/stories.jsx
@@ -0,0 +1,29 @@
+import { SectionBackground } from '.';
+
+export default {
+  title: 'SectionBackground',
+  component: SectionBackground,
+  args: {
+    children: (
+      <div>
+        <h1>SectionBackground</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus dolorum corporis hic. Laboriosam
+          debitis et deserunt? Facere vel officia placeat, perspiciatis quas provident, saepe dicta molestias magni,
+          cumque repudiandae.
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionBackground {...args} />
+    </div>
+  );
+};
