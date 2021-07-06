e6e52c43a27cb1c47fb3b903846f98170d817610
Mauricio Sousa <morricedev@gmail.com>
2021-07-06 18:12:47 -0300

    Created GridTwoColumns component

---
+++ src/components/GridTwoColumns/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid two columns',
+  text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus odit omnis natus dolores ut optio aliquam impedit similique, accusamus voluptate asperiores aperiam minus obcaecati? Aut atque iure eveniet quidem labore.',
+  srcImg: 'assets/images/javascript.svg',
+};
