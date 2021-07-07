6491b2bb61f548ef2bed7367eff666c04038f6d8
Mauricio Sousa <morricedev@gmail.com>
2021-07-07 13:46:58 -0300

    Created GridImage component

---
+++ src/components/GridImage/index.jsx
@@ -0,0 +1,38 @@
+import P from 'prop-types';
+
+import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridImage = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading colorDark={!background} size="huge" uppercase as="h2">
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((el) => (
+            <Styled.GridElement key={`${el.altText}${el.altText}`}>
+              <Styled.Image src={el.srcImg} alt={el.altText} />
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridImage.propTypes = {
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  grid: P.arrayOf(
+    P.shape({
+      altText: P.string.isRequired,
+      srcImg: P.string.isRequired,
+    }),
+  ).isRequired,
+  background: P.bool,
+};
