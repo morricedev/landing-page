cb872a44223cc15c78b8a9497a598ec75907f945
Mauricio Sousa <morricedev@gmail.com>
2021-07-07 13:47:10 -0300

    Created tests for GridImage

---
+++ src/components/GridImage/GridImage.test.jsx
@@ -0,0 +1,15 @@
+import { renderTheme } from '../../styles/renderTheme';
+import { GridImage } from '.';
+import mock from './mock';
+
+describe('<GridImage />', () => {
+  it('should render GridImage with background', () => {
+    const { container } = renderTheme(<GridImage {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render GridImage without background', () => {
+    const { container } = renderTheme(<GridImage {...mock} background={undefined} />);
+    expect(container).toMatchSnapshot();
+  });
+});
