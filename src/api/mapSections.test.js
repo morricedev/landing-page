a997af3056abd2db30a79d8d5414e941115d27a2
Mauricio Sousa <morricedev@gmail.com>
2021-07-08 14:00:50 -0300

    mapTextGrid with tests

--- src/api/mapSections.test.js
+++ src/api/mapSections.test.js
@@ -1,4 +1,4 @@
-import { mapSectionContent, mapSections, mapSectionTwoColumns } from './mapSections';
+import { mapSectionContent, mapSections, mapSectionTwoColumns, mapTextGrid } from './mapSections';
 
 describe('mapSections', () => {
   it('should render predefined section if no data', () => {
@@ -96,5 +96,69 @@ describe('mapSections', () => {
     expect(data.sectionId).toBe('pricing');
     expect(data.html).toBe('abc');
     expect(data.title).toBe('Pricing');
+  });
+
+  it('should map text grid', () => {
+    const data = mapTextGrid({
+      __component: 'section.section-grid',
+      _id: '602fdf2d540c00269e056174',
+      description: 'abc',
+      title: 'My Grid',
+      text_grid: [
+        {
+          _id: '602fdf2d540c00269e05617c',
+          title: 'Teste 1',
+          description: 'Coisa',
+          __v: 0,
+          id: '602fdf2d540c00269e05617c',
+        },
+        {
+          _id: '602fdf2d540c00269e05617d',
+          title: 'Teste 2',
+          description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
+          __v: 0,
+          id: '602fdf2d540c00269e05617d',
+        },
+        {
+          _id: '602fdf2d540c00269e05617e',
+          title: 'Teste 3',
+          description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
+          __v: 0,
+          id: '602fdf2d540c00269e05617e',
+        },
+      ],
+      image_grid: [],
+      metadata: {
+        background: true,
+        _id: '602fdf2e540c00269e056199',
+        name: 'grid-one',
+        section_id: 'grid-one',
+        __v: 0,
+        id: '602fdf2e540c00269e056199',
+      },
+      __v: 2,
+      id: '602fdf2d540c00269e056174',
+    });
+
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-grid');
+    expect(data.sectionId).toBe('grid-one');
+    expect(data.description).toBe('abc');
+    expect(data.title).toBe('My Grid');
+    expect(data.grid[0].title).toBe('Teste 1');
+    expect(data.grid[0].description).toBe('Coisa');
+  });
+
+  it('should map text grid with no data', () => {
+    const data = mapTextGrid();
+
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.description).toBe('');
+    expect(data.title).toBe('');
+    expect(data.grid).toEqual([]);
   });
 });
