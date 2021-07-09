53bd11a8d20753ff06fe9990d63aa0b98372b7fe
Mauricio Sousa <morricedev@gmail.com>
2021-07-09 10:59:57 -0300

    Fixed null URL on mapMenu

--- src/api/mapMenu.js
+++ src/api/mapMenu.js
@@ -1,11 +1,8 @@
 export const mapMenu = (menu = {}) => {
-  const {
-    open_in_new_tab: newTab = false,
-    logo_text: text = '',
-    logo_link: link = '',
-    logo: { url: srcImg = '' } = '',
-    menu: links = [],
-  } = menu;
+  const { open_in_new_tab: newTab = false, logo_text: text = '', logo_link: link = '', menu: links = [] } = menu;
+
+  const srcImg = menu.logo && menu.logo.url ? menu.logo.url : '';
+
   return {
     newTab,
     text,
