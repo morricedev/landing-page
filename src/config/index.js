a8a6d95849affef300a0d54d1efc48618fda2098
Mauricio Sousa <morricedev@gmail.com>
2021-07-09 16:11:19 -0300

    Config heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://localhost:1337/pages/?slug=',
+  url: 'https://strapi-landing-pages-p.herokuapp.com/pages/?slug=',
   siteName: 'Maurício Sousa',
   defaultSlug: 'landing-page',
 };
