0b8268678dd0b151ce81318dda237e203a778f35
Mauricio Sousa <morricedev@gmail.com>
2021-07-10 13:06:28 -0300

    semantic HTML + updated snapshots

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 import { Container as TextComponent } from '../TextComponent/styles';
 import { Container as SectionContainer } from '../SectionContainer/styles';
 
-export const Container = styled.div`
+export const Container = styled.footer`
   ${({ theme }) => css`
     text-align: center;
     border-top: 0.1rem solid ${theme.colors.mediumGray};
--- src/components/GridContent/styles.js
+++ src/components/GridContent/styles.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.section`
   ${() => css`
     max-width: 58rem;
     margin: 0 auto;
